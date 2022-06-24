71a7954486b471945eb037bd02fad6bab410d12c
pgeovany <pgeovany@dcc.ufrj.br>
2022-06-24 17:37:13 -0300

    refactor: remove top-level database connection. Now routes connect to the database when needed.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import express, { json } from "express";
 import { MongoClient } from "mongodb";
 import cors from "cors";
 import dotenv from "dotenv";
-
-import { userSchema, messageSchema } from "./utils/Schemas.js";
 import saveUser from "./utils/saveUser.js";
 import saveMessage from "./utils/saveMessage.js";
+import { userSchema, messageSchema } from "./utils/Schemas.js";
 
 dotenv.config();
 
@@ -13,8 +12,15 @@ const app = express();
 app.use([cors(), json()]);
 
 const mongoClient = new MongoClient(process.env.MONGO_URI);
-await mongoClient.connect();
-const db = mongoClient.db("bate-papo-uol");
+
+function getDataBase() {
+  mongoClient.connect();
+  return mongoClient.db("bate-papo-uol");
+}
+
+function closeDataBase() {
+  mongoClient.close();
+}
 
 app.post("/participants", async (req, res) => {
   const { name } = req.body;
@@ -27,24 +33,29 @@ app.post("/participants", async (req, res) => {
   }
 
   try {
+    const db = getDataBase();
     const user = await db.collection("participants").findOne({ name });
     if (user) {
       res.sendStatus(409);
+      closeDataBase();
       return;
     }
     saveUser(name, db);
     res.sendStatus(201);
   } catch (error) {
     res.status(500).send("Erro ao se conectar ao chat!");
+    closeDataBase();
   }
 });
 
 app.get("/participants", async (req, res) => {
   try {
+    const db = getDataBase();
     const participants = await db.collection("participants").find().toArray();
     res.send(participants);
   } catch (error) {
     res.status(500).send("Erro ao carregar a lista de participantes!");
+    closeDataBase();
   }
 });
 
@@ -52,33 +63,28 @@ app.post("/messages", async (req, res) => {
   const { user } = req.headers;
 
   try {
+    const db = getDataBase();
     const userExists = await db
       .collection("participants")
       .findOne({ name: user });
 
     if (!userExists) {
       res.sendStatus(422);
+      closeDataBase();
       return;
     }
+
+    await messageSchema.validateAsync({
+      ...req.body,
+      from: user,
+    });
+
+    saveMessage({ ...req.body, from: user }, db);
+    res.sendStatus(201);
   } catch (error) {
     res.sendStatus(422);
-    return;
+    closeDataBase();
   }
-
-  const message = {
-    ...req.body,
-    from: user,
-  };
-
-  try {
-    await messageSchema.validateAsync(message);
-  } catch (error) {
-    res.sendStatus(422);
-    return;
-  }
-
-  saveMessage(message, db);
-  res.sendStatus(201);
 });
 
 app.listen(process.env.PORT, () => {
--- src/utils/Schemas.js
+++ src/utils/Schemas.js
@@ -1,14 +1,14 @@
 import joi from "joi";
 
 const userSchema = joi.object({
-  name: joi.string().required(),
+  name: joi.string().trim().required(),
 });
 
 const messageSchema = joi.object({
-  to: joi.string().required(),
-  text: joi.string().required(),
+  to: joi.string().trim().required(),
+  text: joi.string().trim().required(),
   type: joi.any().valid("message", "private_message").required(),
-  from: joi.string().required(),
+  from: joi.string().trim().required(),
 });
 
 export { userSchema, messageSchema };
